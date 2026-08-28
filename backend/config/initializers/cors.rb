45ee8b619b8208e09a1a0188b0ed2891ff1cbf65
Felipe Costa <felipe.sccosta@ufape.edu.br>
2026-08-28 03:36:34 -0300

    feat: adiciona barra de pesquisa de tarefas (closes #15) e corrige bloqueio de CORS

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV.fetch("FRONTEND_ORIGIN", "http://localhost:3001")
+    origins 'http://localhost:3000'
 
-    resource "*",
+    resource '*',
       headers: :any,
-      methods: %i[get post put patch delete options head],
-      credentials: false
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
