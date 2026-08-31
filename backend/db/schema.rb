3d6bc1ed0915db752007a0fca51dcd92b5b21a0d
Felipe Costa <felipe.sccosta@ufape.edu.br>
2026-08-31 15:14:25 -0300

    feat: adiciona múltiplos status de tarefas, painel analítico ajustado e correção de timezone

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_30_021025) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_31_150222) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -48,9 +48,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_08_30_021025) do
     t.string "recurrence_type", default: "single"
     t.boolean "recurring", default: false, null: false
     t.string "recurring_days", default: [], array: true
+    t.string "status", default: "pendente"
     t.string "title"
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
